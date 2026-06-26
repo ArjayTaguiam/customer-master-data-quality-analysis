1d4dff37ff64929a70f2a1cdd6b1d4540dc5e29d
ArjayTaguiam <actaguiam07@gmail.com>
2026-06-26 15:37:36 +0800

    Create 01_create_table.sql

---
+++ data/sql/01_create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE customers (
+    customer_id INT,
+    full_name VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    city VARCHAR(50),
+    source_system VARCHAR(50)
+);
